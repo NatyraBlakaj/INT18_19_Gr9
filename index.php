11a680d50602b9c738f555e393c9166db65d7c94
jblerta <48751197+jblerta@users.noreply.github.com>
2019-05-24 17:19:53 +0200

    validation,signup,gallery...

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" manifest="offline.appcache">
+    <head>
+        <meta charset="UTF-8">
+        <title>Memories</title>
+
+        <link rel="stylesheet" href="css/slider.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/header.css">
+        <link rel="stylesheet" href="css/footer.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+    </head>
+
+
+<body>
+
+        
+        
+       <header>
+            <table id="logoArea">
+                <td id="logo">
+                    <img src="fotot/logo.png" height="100px" width="300px" alt="Logo">
+                </td>
+                <td id="moreOptions">
+                    <a href="src/login.html">Sign up</a> |
+                    <a href="src/login.html">Login</a> |
+                    <a href="src/gallery.html">Archives</a> |
+                    <a href="src/contact.html">Contact</a>
+                </td>
+                <td id="searchBar">
+                    <form action="0">
+                        <input type="search" size="15" autocomplete="on">
+                        <i class="fa fa-search"></i>
+                    </form>
+                </td>
+            </table>
+
+            <div class="mainMenu">
+                <a href="../index.html"   class="active">HOMEPAGE</a>
+                <a href="src/gallery.php"  >GALLERY</a>
+                <a href="src/about.php">ABOUT</a>
+                <a href="src/portfolio.php">PORTFOLIO</a>
+                <a href="src/team.php"  >TEAM</a>
+                <a href="src/contact.php"  >CONTACT</a>
+                <a href="src/game.php" target="blank" >GAME</a>
+                <a href="src/login.php"> LOG IN</a>
+            </div>
+        </header>
+		
+
+
+        <div id="container">
+            <div id="homePageInfo">
+                <div id="slideShow">
+                        <div class="slideshow-container">
+
+                                <div class="mySlides fade">
+                                  <img src="fotot/articlefoto1.jpg" >
+                                  <div class="caption"><h1>Slide 1</h1><p> slide 1 caption</p></div>
+                                </div>
+                                
+                                <div class="mySlides fade">
+                                  <img src="fotot/articlefoto2.jpg">
+                                  <div class="caption">
+                                  <h1>Slide 2</h1>
+                                  <p>
+                                  just some text 
+                                  </p>
+                                  </div>
+                                </div>
+                                
+                                <div class="mySlides fade">
+                                  <img src="fotot/articlefoto3.jpg">
+                                  <div class="caption"><H1>Slide 3</H1><P>this is slide 3 caption</P></div>
+                                </div>
+                                
+                                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                                
+                                </div>
+                                <br>
+                                
+                                
+
+
+                </div>
+               
+                <div id="slideShowText">
+                    <div>
+                        <span class="fadingLine"></span>
+                    </div>
+                        <p>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit aliquid quisquam totam aliquam nobis,<br />
+                            reprehenderit autem natus provident atqu ,, </p>
+                    <div>
+                        <span class="fadingLine"></span>
+                    </div>
+                </div>
+
+            </div>
+        
+            <div id="articles">
+                <article id="article1">
+                    <img src="fotot/spring.jpg" height="180px" width="225px" alt="Pa foto...">
+                    <h3>Lorem ipsum</h3>
+                    <br />
+                    <p> Fugit aliquid quisquam totam aliquam nobis,
+                        reprehenderit autem natus provident atqu</p>
+                    <br />
+                    <a href="src/gallery.html">VIEW THIS CATEGORY >></a>
+                </article>
+
+                <article id="article2">
+                        <img src="fotot/summer.jpg" height="180px" width="225px" alt="Pa foto...">
+                        <h3>Lorem ipsum</h3>
+                        <br />
+                        <p> Fugit aliquid quisquam totam aliquam nobis,
+                            reprehenderit autem natus provident atqu</p>
+                        <br />
+                        <a href="src/gallery.html">VIEW THIS CATEGORY >></a>
+                </article>
+                
+                <article id="article3">
+                            <img src="fotot/autumn.jpg" height="180px" width="225px" alt="Pa foto...">
+                            <h3>Lorem ipsum</h3>
+                            <br />
+                            <p> Fugit aliquid quisquam totam aliquam nobis,
+                                reprehenderit autem natus provident atqu</p>
+                            <br />
+                            <a href="src/gallery.html">VIEW THIS CATEGORY >></a>
+                </article>
+                    
+                <article id="article4">
+                                <img src="fotot/winter.jpg" height="180px" width="225px" alt="Pa foto...">
+                                <h3>Lorem ipsum</h3>
+                                <br />
+                                <p> Fugit aliquid quisquam totam aliquam nobis,
+                                    reprehenderit autem natus provident atqu</p>
+                                <br />
+                                <a href="src/gallery.html">VIEW THIS CATEGORY >></a>
+                </article>
+
+            </div>
+        </div>
+        <div  id="moreInfo">
+                <div id="developers">
+                    <h2>Punuan</h2>
+                    <div class="developer">
+                            <table>
+                                    <tr>
+                                    <td rowspan="3" colspan="5"><img class="devAvatar" height="95px" width="95px" src="fotot/maleavatar.jpg"></td>
+                                    </tr>
+                                    <tr>
+                                      <td><b>Shkelqim Maxharraj</b></td>
+                                    </tr>
+                                    <tr>
+                                      <td>disa tekst rreth ketij puntori te zellsisa tekst rreth disa tekst rreth ketij puntori te zellsisa tekst r ketij puntori te zellsh</td>
+                                    </tr>
+                            </table>
+                            <table>
+                                    <tr>
+                                    <td rowspan="3"><img class="devAvatar" height="95px" width="95px" src="fotot/femaleavatar.jpg"></td>
+                                    </tr>
+                                    <tr>
+                                      <td><b>Natyra Blakaj</b></td>
+                                      
+                                    </tr>
+                                    <tr>
+                                      <td>disa tekst rreth ketij disa tekst rreth ketij puntori te zellsisa tekst puntori te zellsisa tekst rreth ketij puntori te zellsh</td>
+                                    </tr>
+                            </table>
+                    </div>
+
+                </div>
+                <div id="aboutProject">
+                    <h3>One quarter</h3>
+                    <br />
+                    <p><b>Post title</b> <br /> <small><a href="#">Admin</a>domain.com<br />
+                    Thursday, 22nd November, 2018
+                    </small>
+                    <br /><br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Consequuntur aperiam at tempore officiis esse quod<br />
+                     <a href="src/team.html" class="readMore">Read more</a>
+                    </p>
+                        <br />
+                    <p><b>Post title</b> <br /> <small><a href="#">Admin</a>domain.com<br />
+                        Thursday, 22nd November, 2018
+                        </small>
+                        <br /><br />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                         Consequuntur aperiam at tempore officiis esse quod<br />
+                         <a href="src/team.html" class="readMore">Read more</a>
+                    </p>
+                    <br />
+                </div>
+                <div id="submitForm">
+                    <h3>One quarter</h3>
+                        <br />
+                
+                    <form action="">
+                        <input size="27" placeholder="Name" type="text">
+                        <input size="27" placeholder="Email" type="email">
+                        <textarea rows="7" placeholder="Message" cols="28"></textarea>
+                        <button type="submit">Submit</button>
+                    </form>
+                    <br />
+                </div>
+        </div>
+
+        <footer>
+            <pre>                                                       copyright&copy;2013-All rights Reserved-Domain Name                                                                                                                                                                                                                     Punoi grupi 9</pre>
+        </footer>
+
+    <script src="js/responsive.js"></script>
+    <script>
+            var x = 1;
+            showSlides(x);
+
+            function plusSlides(n) {
+            showSlides(x += n);
+            }
+
+
+            function currentSlide(n) {
+            showSlides(x = n);
+            }
+
+            function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {x = 1}    
+            if (n < 1) {x = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[x-1].style.display = "block";  
+            dots[x-1].className += " active";
+            }
+    
+    </script>
+</body>
+</html>
---
+++ src/gallery.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="UTF-8">
+        <title>Memories</title>
+
+
+        <link rel="stylesheet" href="../css/header.css">
+        <link rel="stylesheet" href="../css/main.css">
+        <link rel="stylesheet" href="../css/login.css">
+		<link rel="stylesheet" href="../css/gallery.css">
+        <link rel="stylesheet" href="../css/footer.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+
+<body>
+
+        <?php
+		include "header.php";
+		?>
+    <section class="gallery-links">
+   <div class="wrapper">
+   
+   <h2>Gallery</h2>
+   <hr class="line">
+   <div class="gallery-container">
+ <?php
+   if (isset($_SESSION['IDperdoruesit'])){     
+
+   include_once 'includes/dbh.inc.php';
+   
+   $sql="select * from gallery order by orderGallery DESC;";
+   $stmt=mysqli_stmt_init($conn);
+   if(!mysqli_stmt_prepare($stmt,$sql)){
+	   echo "SQl statement failes!";
+   }else{
+	   mysqli_stmt_execute($stmt);
+	   $result=mysqli_stmt_get_result($stmt);
+	   
+	   while($row=mysqli_fetch_assoc($result)){
+		   echo '
+		   <div class="foto">
+     <a href="#">
+	 <div style="background-image:url(img/gallery/'.$row["imgFullNameGallery"].');"></div>
+	   <h3>'.$row["titleGallery"].'</h3>
+	 <p>'.$row["descGallery"].'</p>
+	 </a>
+	 </div>';
+	   }
+   }
+
+   
+   echo'
+   <div class="gallery-upload">
+   <h1>Uploadfile</h1>
+   <form action="includes/gallery-upload.inc.php" method="post" enctype="multipart/form-data" >				
+   <input type="text" name="filename" placeholder="File name...">
+   <input type="text" name="filetitle" placeholder="Image title...">
+   <input type="text" name="filedesc" placeholder="Image desctription...">
+   <input type="file" name="file">
+   <button type="submit" name="submit">Upload</button>
+   </form>
+   </div>';
+  
+
+   
+   
+   }
+     else {
+	   echo '
+ 
+   </div>
+      </div>
+   </section>
+  
+    <div class="container">
+  
+            <article class="landscape">
+                <img src="../fotot/memory1.jpg" alt="Foto nuk eshte loaduar" height="200px" width="300px">
+                <h1>Memory 1</h1>
+                <p>A trip to cyprus</p>
+            </article>
+            <article clas="landscape">
+                <img src="../fotot/memory2.jpg" alt="Foto nuk eshte loaduar" height="200px" width="300px">
+                <h1>Memory 2</h1>
+                <p>A trip to cyprus</p>
+            </article>
+            <article class="landscape">
+                <img src="../fotot/memory4.jpg" alt="Foto nuk eshte loaduar" height="200px" width="300px">
+                <h1>Memory 3</h1>
+                <p>A trip to cyprus</p>
+            </article>
+            <article class="landscape">
+                <img src="../fotot/memory7.jpg" alt="Foto nuk eshte loaduar" height="200px" width="300px">
+                <h1>Memory 4</h1>
+                <p>A trip to cyprus</p>
+            </article>
+            <article class="city">
+                <img src="../fotot/memory3.jpg" alt="Foto nuk eshte loaduar" height="200px" width="300px">
+                <h1>Memory 5</h1>
+                <p>A trip to cyprus</p>
+            </article>
+            <article class="arch">
+                <img src="../fotot/memory5.jpg" alt="Foto nuk eshte loaduar" height="200px" width="300px">
+                <h1>Memory 6</h1>
+                <p>A trip to cyprus</p>
+            </article>
+            <article class="arch">
+                <img src="../fotot/memory6.jpg" alt="Foto nuk eshte loaduar" height="200px" width="300px">
+                <h1>Memory 7</h1>
+                <p>A trip to cyprus</p>
+            </article>
+            <article class="other">
+                <img src="../fotot/memory8.jpg" alt="Foto nuk eshte loaduar" height="200px" width="300px">
+                <h1>Memory 8</h1>
+                <p>A trip to cyprus</p>
+            </article>
+            <article class="other">
+                <img src="../fotot/memory9.jpg" alt="Foto nuk eshte loaduar" height="200px" width="300px">
+                <h1>Memory 9</h1>
+                <p>A trip to cyprus</p>
+            </article>
+        </div>';
+	 }
+		?>
+        <footer>
+            <pre>                                                       copyright&copy;2013-All rights Reserved-Domain Name                                                                                                                                                                                                                     Punoi grupi 9</pre>
+        </footer>
+
+    <script>
+    $('input:radio[name="type"]').change(
+    function(){
+        if ($(this).is(':checked') && $(this).val() == 'landscape') {
+            $(".landscape").show();
+            $(".city").hide();
+            $(".arch").hide();
+            $(".other").hide();
+        }
+        else if ($(this).is(':checked') && $(this).val() == 'city') {
+            $(".city").show();
+            $(".landscape").hide();
+            $(".arch").hide();
+            $(".other").hide();
+        }
+        else if ($(this).is(':checked') && $(this).val() == 'arch') {
+            $(".arch").show();
+            $(".landscape").hide();
+            $(".city").hide();
+            $(".other").hide();
+        }
+        else if ($(this).is(':checked') && $(this).val() == 'other') {
+            $(".other").show();
+            $(".city").hide();
+            $(".arch").hide();
+            $(".landscape").hide();
+        }
+    });
+    </script>
+</body>
+</html>
